ae820c1b6e097c5aab18addfd4919397f3747c99
Mark-andrew7 <kamaumark19@gmail.com>
2024-05-07 13:04:48 +0300

    Fixed pycodestyle issues

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,23 +4,21 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-"""
-Returns number of subscribers
-"""
+    """
+    Returns number of subscribers
+    """
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
 
-if subreddit is None or not isinstance(subreddit, str):
-    return 0
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    user_agent = "User-agent": "custom"
+    response = requests.get(url, header=user_agent)
+    res = response.json()
 
-url = f"https://www.reddit.com/r/{subreddit}/about.json"
-user_agent = "User-agent": "custom"
-response = requests.get(url, header=user_agent)
-res = response.json()
+    try:
+        data = res.requests.get("data")
+        subs = data.requests.get("subsribers")
+        return subs
 
-try:
-    data = res.requests.get("data")
-    subs = data.requests.get("subsribers")
-    return subs
-
-except Exception:
-    return 0
-
+    except Exception:
+        return 0
