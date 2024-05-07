987d563b2e5314a9bde04c80efc06afd7f42a27e
Mark-andrew7 <kamaumark19@gmail.com>
2024-05-07 12:54:08 +0300

    How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+"""
+Returns number of subscribers
+"""
+
+if subreddit is None or not isinstance(subreddit, str):
+    return 0
+
+url = f"https://www.reddit.com/r/{subreddit}/about.json"
+user_agent = "User-agent": "custom"
+response = requests.get(url, header=user_agent)
+res = response.json()
+
+try:
+    data = res.requests.get("data")
+    subs = data.requests.get("subsribers")
+    return subs
+
+except Exception:
+    return 0
+
