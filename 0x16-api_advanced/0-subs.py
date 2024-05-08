d6e8296a76216c8bf1e48ee0c54b3b4d0b80dc9e
Mark-andrew7 <kamaumark19@gmail.com>
2024-05-08 08:32:35 +0300

    Added status code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,14 +14,14 @@ def number_of_subscribers(subreddit):
         return 0
 
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    user_agent = {'User-agent': 'custom'}
-    response = requests.get(url, header=user_agent)
-    res = response.json()
-
-    try:
-        data = res.requests.get('data')
-        subs = data.requests.get('subsribers')
-        return subs
-
-    except Exception:
+    user_agent = {'User-Agent': 'custom'}
+    response = requests.get(url, headers=user_agent)
+    if response.status_code == 200:
+        try:
+            data = response.json()['data']
+            subs = data['subscribers']
+            return subs
+        except KeyError:
+            return 0
+    else:
         return 0
